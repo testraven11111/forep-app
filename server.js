b3fb416187d68c5b6c7af6f68b4579527c6b3602
Galih Tripambudi <87571952+testraven11111@users.noreply.github.com>
2023-06-20 19:38:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,24 @@
 const Hapi = require('@hapi/hapi');
-const fs = require('fs');
 const path = require('path');
 
 const server = Hapi.server({
-    port: process.env.PORT || 8000,
-    host: '0.0.0.0',
+    port: 8000,
+    host: 'localhost',
 });
 
 server.route({
     method: 'GET',
     path: '/api/resep',
     handler: (request, h) => {
-        const resepPath = path.join(__dirname, './resep.json');
-        const resepData = fs.readFileSync(resepPath, 'utf8');
-        const resepJSON = JSON.parse(resepData);
-        return resepJSON;
+        const resepPath = path.join(__dirname, './src/reseplist.js');
+        const resepData = require(resepPath);
+        return resepData;
     },
 });
 
-const startServer = async () => {
-    try {
-        await server.start();
-        console.log(`Server berjalan pada ${server.info.uri}`);
-    } catch (error) {
-        console.error('Terjadi kesalahan saat memulai server Hapi:', error);
-        process.exit(1);
-    }
+const init = async () => {
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
-startServer();
+init();
