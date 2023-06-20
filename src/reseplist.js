3402e08610b4b8beacd575a3d6ad7e8f9d321d86
Galih Tripambudi <87571952+testraven11111@users.noreply.github.com>
2023-06-20 19:40:03 +0700

    Create reseplist.js

---
+++ src/reseplist.js
@@ -0,0 +1,37 @@
+const resep = [
+    {
+        id: 1,
+        daerah: 'Bandung',
+        nama_resep: 'Sate Maranggi',
+        bahan: ['Daging sapi', 'Kecap manis', 'Bawang merah'],
+        cara_membuat: [
+            'Tusuk daging sapi kecil-kecil.',
+            'Lumuri dengan kecap manis dan bawang merah.',
+            'Bakar hingga matang.',
+        ],
+    },
+    {
+        id: 2,
+        daerah: 'Yogyakarta',
+        nama_resep: 'Gudeg Jogja',
+        bahan: ['Nangka muda', 'Santan', 'Gula jawa', 'Daun salam'],
+        cara_membuat: [
+            'Rebus nangka muda hingga empuk.',
+            'Tambahkan santan, gula jawa, dan daun salam.',
+            'Masak hingga kuah kental.',
+        ],
+    },
+    {
+        id: 3,
+        daerah: 'Padang',
+        nama_resep: 'Rendang',
+        bahan: ['Daging sapi', 'Santan', 'Bumbu rendang'],
+        cara_membuat: [
+            'Tumis bumbu rendang hingga harum.',
+            'Masukkan daging sapi dan santan.',
+            'Masak hingga kuah menyusut dan daging empuk.',
+        ],
+    },
+];
+
+export default resep;
