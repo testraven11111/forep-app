98c745dc89a1e8eec1713e7d4853e67fe7c0c849
Galih Tripambudi <87571952+testraven11111@users.noreply.github.com>
2023-06-20 21:39:45 +0700

    Update Resep.jsx

--- src/pages/Resep.jsx
+++ src/pages/Resep.jsx
@@ -1,13 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import resepData from '../reseplist';
 
 const Resep = () => {
     const [resep, setResep] = useState([]);
 
     useEffect(() => {
-        setResep(resepData);
+        fetchResep();
     }, []);
+
+    const fetchResep = async () => {
+        try {
+            const response = await fetch('http://localhost:8000/api/resep');
+            if (response.ok) {
+                const resepData = await response.json();
+                setResep(resepData);
+            } else {
+                console.log('Terjadi kesalahan saat mengambil data resep');
+            }
+        } catch (error) {
+            console.log('Terjadi kesalahan saat mengambil data resep', error);
+        }
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            const response = await fetch(`http://localhost:8000/api/resep/${id}`, {
+                method: 'DELETE',
+            });
+            if (response.ok) {
+                const updatedResep = resep.filter((item) => item.id !== id);
+                setResep(updatedResep);
+            } else {
+                console.log('Terjadi kesalahan saat menghapus resep');
+            }
+        } catch (error) {
+            console.log('Terjadi kesalahan saat menghapus resep', error);
+        }
+    };
 
     return (
         <div>
@@ -15,7 +44,8 @@ const Resep = () => {
             {resep.map((item) => (
                 <div key={item.id}>
                     <h2>{item.nama_resep}</h2>
-                    <p>Provinsi: {item.daerah}</p>
+                    <p>Provinsi: {item.provinsi}</p>
+                    <p>Daerah: {item.daerah}</p>
                     <p>Bahan: {item.bahan.join(', ')}</p>
                     <p>Cara Membuat:</p>
                     <ol>
@@ -24,6 +54,7 @@ const Resep = () => {
                         ))}
                     </ol>
                     <Link to={`/resep/${item.id}`}>Lihat Detail</Link>
+                    <button onClick={() => handleDelete(item.id)}>Delete</button>
                 </div>
             ))}
         </div>
